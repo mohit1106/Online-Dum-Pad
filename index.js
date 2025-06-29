bc38c1db35a6f7c42d839d1950fba905e2d2e41c
Mohit Verma <118046944+mohit1106@users.noreply.github.com>
2025-06-29 15:03:18 +0530

    index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 ///////////////////////////////////// detecting the clicks on buttons
 for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+    
     document.querySelectorAll(".drum")[i].addEventListener("click", function() {
         var buttonInnerHTML = this.innerHTML;
         makeSound(buttonInnerHTML);
         buttonAnimation(buttonInnerHTML);
-    });
+    } );
 }
 
 //////////////////////////// detecting the keyboard presses
